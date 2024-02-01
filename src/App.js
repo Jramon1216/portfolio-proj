117969a23eebaaf264f6bf43cfcb41c3b04edb52
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-02-01 14:15:38 -0500

    added page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {Routes, Route, Router} from 'react-router-dom';
 import './App.css';
+import { Nav, About, Skills, Contact } from './views'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav/>
+          <Routes>
+            <Route path="/" element={<About/>}/>
+            <Route path="/skills" element={<Skills/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+    </>
   );
 }
 
---
+++ src/views/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Nav(){
+    return(
+        <nav>
+            <ul>
+                <li><Link to="/">About Me</Link></li>
+                <li><Link to="/skills">Skills</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    )
+}
