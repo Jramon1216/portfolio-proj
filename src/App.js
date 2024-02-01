c3cd6a2691b40f56f38ed41a6f8b427de002cd6f
Jonathan Ramon <jramon@afsenyc.org>
2024-01-31 20:52:52 -0500

    Added routing to the project
    Created views directory for the different pages
    
    Created index.js file in views direcotry to centralize imports for App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
+import {About, Contact, Skills, Nav} from './views';
+// import assets here (icons etc) 
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<About/>}/>
+        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/contact" element={<Contact/>}/> 
+      </Routes>
+    </>
   );
 }
 
-export default App;
