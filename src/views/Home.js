43dc58013d51dc783ea678bf3ce7a2e8f59b5046
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-02-01 23:03:53 -0500

    - Added Template for Home page - Moved scss files to their own directory

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,20 @@ import React from "react";
 
 export default function Home() {
     return(
-        <></>
+        <>
+            <article>
+                <h2 className="sub-heading">Hello my name is Jonathan Ramon</h2>
+                <p className="article-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                    Proin a justo sed ex bibendum hendrerit. Duis vulputate urna in justo facilisis, vel laoreet quam sollicitudin. 
+                    Sed non neque id augue fermentum interdum. Nunc pharetra justo id sapien iaculis, eu congue eros fermentum. 
+                    In hac habitasse platea dictumst. Vivamus nec ex vel neque eleifend auctor ac sed leo. 
+                    Ut ut mauris vitae mi volutpat suscipit.Quisque vitae felis in velit rhoncus feugiat eu id orci. Sed quis fermentum justo.
+                </p>
+            </article>
+
+            </>
     )
 }
