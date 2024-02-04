346ccaa45cac688dbcc2e32cdcc7a1dd38e8bb18
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-02-04 14:46:45 -0500

    Added structure to the About page

--- src/views/About.js
+++ src/views/About.js
@@ -5,6 +5,21 @@ export default function About(){
     return(
         <>
             <h1>About Me</h1>
+            <section className='aboutMain'>
+                <img src ="" alt='some generic coding image'/>
+                {/* Center this */}
+                <p>Some more Lorem ipsum stuff</p>
+            </section>
+            <section className='jobExperience'>
+                <p>Workplace expeience 1</p>
+                {/* These should be inline */}
+                <img src="" alt="place holder rab image"/>
+            </section>
+            <section className="jobExperience">
+                <p>Workplace expeience 2</p>
+                {/* These should be inline */}
+                <img src="" alt="place holder morgan stanley image"/>
+            </section>
         </>
     )
 }
