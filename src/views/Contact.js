b55ed0d784395e965d538eb6121e213bccaeaa61
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-02-04 17:27:21 -0500

    -created structure for the Contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import '../styles/Contact.module.scss'
+import styles from '../styles/Contact.module.scss'
 
 export default function Contact(){
     return(
         <>
             <h1>Contact Me!</h1>
+            <section>
+                <div className={styles.imgContainer}>
+                    <img src="" alt="LinkedIn Logo"/>
+                    <img src="" alt="Indeed Logo"/>
+                    <img src="" alt="Email Icon"/>
+                </div>
+            </section>
+            <hr width={1000}></hr>
+            <section>
+                <div className={styles.textContainer}>
+                    <p>Lorem ipsum stuff</p>
+                </div>
+            </section>
+
         </>
     )
 }
