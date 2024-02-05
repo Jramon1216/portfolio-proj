059a9031fd38bf7d40794d18ee8ffe05f4c7eccf
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-02-04 20:08:50 -0500

    - added page structure for Projects page

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import '../styles/Projects.module.scss'
+import styles from '../styles/Projects.module.scss'
 
 export default function Projects(){
     return(
         <>
             <h1>My Projects</h1>
+            <section className={styles.projectEntry}>
+                <div className={styles.textContainer}>
+                    <h2>Project</h2>
+                    <p>
+                        Lorem ipsum stuff
+                    </p>
+                </div>
+                <div className={styles.imageContainer}>
+                    <img src="" alt="some project image"/>
+                </div>
+                <div className={styles.svgLogos}>
+                    <img src="" alt='svg logos'/>
+                </div>
+            </section>
         </>
     )
 }
