b976b52d4760b4d958a9a6a1ec77dd4fd8886a8f
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-05-07 15:06:28 -0400

    Added content and styling to home page and contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -15,7 +15,11 @@ export default function Contact(){
             <hr width={1000}></hr>
             <section>
                 <div className={styles.textContainer}>
-                    <p>Lorem ipsum stuff</p>
+                    <p>
+                        Thank you for visiting my webpage. If you're interested in connecting with me, please feel free to use the 
+                        provided links above. I eagerly anticipate the opportunity to connect with you and discuss potential collaborations or opportunities.
+                        I am looking forward to hearing from you soon!
+                    </p>
                 </div>
             </section>
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,29 +5,26 @@ import styles from '../styles/Home.module.scss'
                                     MY PROJECTS SECTION
  * In the future consider making the projects section a child component of Home 
  * Maybe the Homepage can have 3 of your best projects and a link to the rest of the list in a seperate component
- * Animations for toggling the details elements would be nice
+ * Look into a dark mode feature 
 */
 
 /**
- *  TODO: Finish adding class names for the rest of the elements
- *  TODO: Finish structure styling for the Skills and My Projects sections 
  *
  */
 
 export default function Home() {
     return(
         <>
+            <img src="https://placehold.co/1000x400" className={styles.aboutMeImg} alt=""/>
         <section>
             <div className={styles.aboutMeContainer}>
                 <h2 className={styles.subHeading}>Hello my name is Jonathan Ramon</h2>
-                <p className={styles.articleText}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                    Proin a justo sed ex bibendum hendrerit. Duis vulputate urna in justo facilisis, vel laoreet quam sollicitudin. 
-                    Sed non neque id augue fermentum interdum. Nunc pharetra justo id sapien iaculis, eu congue eros fermentum. 
-                    In hac habitasse platea dictumst. Vivamus nec ex vel neque eleifend auctor ac sed leo. 
-                    Ut ut mauris vitae mi volutpat suscipit.Quisque vitae felis in velit rhoncus feugiat eu id orci. Sed quis fermentum justo.
+                <p className={styles.aboutMeText}>
+                    Welcome to my webpage! I'm a dedicated web/software developer with nearly two years of experience seeking new opportunities in the field. 
+                    My passion for coding drives me to continually expand my skill set by exploring various technologies. 
+                    From mastering languages like JavaScript and frameworks like React to delving into backend development with Python Django, I thrive on learning and implementing new tools to enhance my abilities. 
+                    With a strong foundation in web development, I am committed to delivering high-quality solutions and contributing effectively to projects. 
+                    Explore my portfolio to see examples of my work and get in touch to discuss potential collaborations or job opportunities. 
                 </p>
             </div>
         </section>
@@ -36,29 +33,68 @@ export default function Home() {
                 <h2>My Projects</h2>
                 <details>
                     <summary>Sample Project</summary>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. </p>
+                    <table>
+                        <tbody>
+                            <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
+                            <td>
+                                <p className={styles.projDesc}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                                </p>
+                            </td>
+                        </tbody>
+                    </table>
                 </details>
                 <details>
                     <summary>Sample Project</summary>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. </p>
+                    <table>
+                        <tbody>
+                            <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
+                            <td>
+                                <p className={styles.projDesc}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                                </p>
+                            </td>
+                        </tbody>
+                    </table>
                 </details>
                 <details>
                     <summary>Sample Project</summary>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. </p>
+                    <table>
+                        <tbody>
+                            <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
+                            <td>
+                                <p className={styles.projDesc}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                                </p>
+                            </td>
+                        </tbody>
+                    </table>
                 </details>
             </div>
         </section>
-            <h2>Skills</h2>
-            <code>SVG'S GO HERE</code>
+        <section>
+            <div>
+                
+            </div>
+        </section>
+        <section>
+            
+        </section>
+        <section>
+            <div>
+                <h2>Skills</h2>
+                <code>SVG'S GO HERE</code>
+                {
+                    //https://www.svgrepo.com/
+                }
+            </div>
+        </section>
         </>
     )
 }
