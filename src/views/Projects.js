b1be9b9ae2c87315cf324daa8319de07b791c7e8
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-05-07 15:00:39 -0400

    Fixed positioning of elements on the project page

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -5,11 +5,19 @@ export default function Projects(){
     return(
         <>
             <h1>My Projects</h1>
+            <section>
+                <div className={styles.projectIntro}>
+                    <h2>Projects I've worked on</h2>
+                    {/* <p>Lorem ipsum stuff</p> */}
+                </div>
+            </section>
             <section className={styles.projectEntry}>
+                <h2>Project</h2>
                 <div className={styles.textContainer}>
-                    <h2>Project</h2>
                     <p>
-                        Lorem ipsum stuff
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
                     </p>
                 </div>
                 <div className={styles.imageContainer}>
