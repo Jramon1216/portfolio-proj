7a094e0e71d773f1e1e6233ad534a5477d1f2a26
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-11-10 21:53:15 -0500

    - Removed width property in index.css - Added professional photo png - Converted classes to id's for unique elements

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from '../styles/Home.module.scss'
+import professionalPhoto from '../assets/img/professionalPhoto.png';
 
 /**
                                     MY PROJECTS SECTION
@@ -15,18 +16,18 @@ import styles from '../styles/Home.module.scss'
 export default function Home() {
     return(
         <>
-            <img src="https://placehold.co/1000x400" className={styles.aboutMeImg} alt=""/>
-        <section>
-            <div className={styles.aboutMeContainer}>
-                <h2 className={styles.subHeading}>Hello my name is Jonathan Ramon</h2>
-                <p className={styles.aboutMeText}>
-                    Welcome to my webpage! I'm a dedicated web/software developer with nearly two years of experience seeking new opportunities in the field. 
+        <section id={styles.aboutMeContainer}>
+            <div id={styles.aboutMeDiv}>
+                <h2 id={styles.subHeading}>Hello my name is Jonathan Ramon</h2>
+                <p id={styles.aboutMeText}>
+                    Welcome to my webpage! I'm a dedicated Web/Software developer with nearly two years of experience seeking new opportunities in the field. 
                     My passion for coding drives me to continually expand my skill set by exploring various technologies. 
                     From mastering languages like JavaScript and frameworks like React to delving into backend development with Python Django, I thrive on learning and implementing new tools to enhance my abilities. 
                     With a strong foundation in web development, I am committed to delivering high-quality solutions and contributing effectively to projects. 
                     Explore my portfolio to see examples of my work and get in touch to discuss potential collaborations or job opportunities. 
                 </p>
             </div>
+                <img src={professionalPhoto} id={styles.aboutMeImg} alt="Professional Photo of Jonathan Ramon"/>
         </section>
         <section>
             <div className={styles.projectSection}>
