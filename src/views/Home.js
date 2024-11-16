54bf79d5743bf685a5629c46c01f3ec4322a1aba
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-11-16 13:57:18 -0500

    - added max-width propertey back to index.css - Added max-height property to nav bar - Restrutured about me section; The aboutMeText is now next to aboutMeImg - Created id's and classes for the project section - Moved skill section into aboutMeContainer (will correct alignment in a later commit)

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,15 +3,12 @@ import styles from '../styles/Home.module.scss'
 import professionalPhoto from '../assets/img/professionalPhoto.png';
 
 /**
-                                    MY PROJECTS SECTION
- * In the future consider making the projects section a child component of Home 
- * Maybe the Homepage can have 3 of your best projects and a link to the rest of the list in a seperate component
- * Look into a dark mode feature 
+    MY PROJECTS SECTION
+    * TODO: MOVE TECH SKILLS INTO ABOUT ME CONTAINER 
+    * TODO: FIND CLICK TO EXPAND DESIGN
+    * TODO: START WORK ON CONTACT ME CONTAINER
+    * Look into a light mode feature
 */
-
-/**
- *
- */
 
 export default function Home() {
     return(
@@ -27,80 +24,73 @@ export default function Home() {
                     Explore my portfolio to see examples of my work and get in touch to discuss potential collaborations or job opportunities. 
                 </p>
             </div>
-                <img src={professionalPhoto} id={styles.aboutMeImg} alt="Professional Photo of Jonathan Ramon"/>
-        </section>
-        <section>
-            <div className={styles.projectSection}>
-                <h2>My Projects</h2>
-                <details>
-                    <summary>Sample Project</summary>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
-                                <td>
-                                    <p className={styles.projDesc}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </details>
-                <details>
-                    <summary>Sample Project</summary>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
-                                <td>
-                                    <p className={styles.projDesc}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </details>
-                <details>
-                    <summary>Sample Project</summary>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
-                                <td>
-                                    <p className={styles.projDesc}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </details>
-            </div>
-        </section>
-        <section>
-            <div>
-                
-            </div>
-        </section>
-        <section>
-            
-        </section>
-        <section>
-            <div>
-                <h2>Skills</h2>
+            <div id={styles.skillsDiv}>
+                <h2 id={styles.skillsSubheading}>Skills</h2>
                 <code>SVG'S GO HERE</code>
                 {
                     //https://www.svgrepo.com/
                 }
             </div>
+                <img src={professionalPhoto} id={styles.aboutMeImg} alt="Professional Photo of Jonathan Ramon"/>
+        </section>
+            <section id={styles.projectContianer}>
+            <div>
+                <h2>My Projects</h2>
+                <details>
+                    <summary>Sample Project</summary>
+                    <table>
+                        <tbody>
+                            <tr className={styles.projectRow}>
+                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
+                                <td>
+                                    <p className={styles.projDesc}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </details>
+                <details>
+                    <summary>Sample Project</summary>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
+                                <td>
+                                    <p className={styles.projDesc}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </details>
+                <details>
+                    <summary>Sample Project</summary>
+                    <table>
+                        <tbody>
+                            <tr >
+                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
+                                <td>
+                                    <p className={styles.projDesc}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
+                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </details> 
+            </div>
+           
+        </section>
+        <section>
         </section>
         </>
     )
