4da5eccd5c99342a5aa1013b5c154850b77fdff0
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-11-17 16:36:07 -0500

    - Added @emotion/react and mui library to package.json - Repurposed project page into accordion compnent on the home page - Restructured layout of Home.scss - Turned subHeading from an id to a class

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,97 +1,42 @@
 import React from "react";
 import styles from '../styles/Home.module.scss'
 import professionalPhoto from '../assets/img/professionalPhoto.png';
+import Projects from "./Projects";
 
 /**
     MY PROJECTS SECTION
-    * TODO: MOVE TECH SKILLS INTO ABOUT ME CONTAINER 
-    * TODO: FIND CLICK TO EXPAND DESIGN
     * TODO: START WORK ON CONTACT ME CONTAINER
-    * Look into a light mode feature
+    * Look into a light mode feature (low priority)
 */
 
 export default function Home() {
-    return(
+    return (
         <>
-        <section id={styles.aboutMeContainer}>
-            <div id={styles.aboutMeDiv}>
-                <h2 id={styles.subHeading}>Hello my name is Jonathan Ramon</h2>
-                <p id={styles.aboutMeText}>
-                    Welcome to my webpage! I'm a dedicated Web/Software developer with nearly two years of experience seeking new opportunities in the field. 
-                    My passion for coding drives me to continually expand my skill set by exploring various technologies. 
-                    From mastering languages like JavaScript and frameworks like React to delving into backend development with Python Django, I thrive on learning and implementing new tools to enhance my abilities. 
-                    With a strong foundation in web development, I am committed to delivering high-quality solutions and contributing effectively to projects. 
-                    Explore my portfolio to see examples of my work and get in touch to discuss potential collaborations or job opportunities. 
-                </p>
-            </div>
-            <div id={styles.skillsDiv}>
-                <h2 id={styles.skillsSubheading}>Skills</h2>
-                <code>SVG'S GO HERE</code>
-                {
-                    //https://www.svgrepo.com/
-                }
-            </div>
-                <img src={professionalPhoto} id={styles.aboutMeImg} alt="Professional Photo of Jonathan Ramon"/>
-        </section>
-            <section id={styles.projectContianer}>
-            <div>
-                <h2>My Projects</h2>
-                <details>
-                    <summary>Sample Project</summary>
-                    <table>
-                        <tbody>
-                            <tr className={styles.projectRow}>
-                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
-                                <td>
-                                    <p className={styles.projDesc}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </details>
-                <details>
-                    <summary>Sample Project</summary>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
-                                <td>
-                                    <p className={styles.projDesc}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </details>
-                <details>
-                    <summary>Sample Project</summary>
-                    <table>
-                        <tbody>
-                            <tr >
-                                <td><img src="https://placehold.co/300x200" className={styles.projImg} alt=""/></td>
-                                <td>
-                                    <p className={styles.projDesc}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                                        Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </details> 
-            </div>
-           
-        </section>
-        <section>
-        </section>
+            <section id={styles.aboutMeContainer}>
+                <div id={styles.aboutMeDiv}>
+                    <h2 className={styles.subHeading}>Hello my name is Jonathan Ramon</h2>
+                    <p id={styles.aboutMeText}>
+                        Welcome to my webpage! I'm a dedicated Web/Software developer with nearly two years of experience seeking new opportunities in the field.
+                        My passion for coding drives me to continually expand my skill set by exploring various technologies.
+                        From mastering languages like JavaScript and frameworks like React to delving into backend development with Python Django, I thrive on learning and implementing new tools to enhance my abilities.
+                        With a strong foundation in web development, I am committed to delivering high-quality solutions and contributing effectively to projects.
+                        Explore my portfolio to see examples of my work and get in touch to discuss potential collaborations or job opportunities.
+                    </p>
+                    <h2 className={styles.subHeading}>Skills</h2>
+                    <code>SVG'S GO HERE</code>
+                    {
+                        //https://www.svgrepo.com/
+                    }
+                </div>
+                <img src={professionalPhoto} id={styles.aboutMeImg} alt="Professional Photo of Jonathan Ramon" />
+            </section>
+            <section id={styles.projectsContainter}>
+                <h2 className={styles.subHeading} id={styles.projectsSubheading}>My Projects</h2>
+                <Projects />
+            </section>
+            <section>
+            </section>
+
         </>
     )
 }
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,32 +1,104 @@
-import React from 'react'
-import styles from '../styles/Projects.module.scss'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import styles from '../styles/Projects.module.scss';
 
-export default function Projects(){
-    return(
-        <>
-            <h1>My Projects</h1>
-            <section>
-                <div className={styles.projectIntro}>
-                    <h2>Projects I've worked on</h2>
-                    {/* <p>Lorem ipsum stuff</p> */}
-                </div>
-            </section>
-            <section className={styles.projectEntry}>
-                <h2>Project</h2>
-                <div className={styles.textContainer}>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Nulla facilisi. Integer feugiat, nisl vel ullamcorper vestibulum, tortor sapien tristique dolor, sit amet congue nisl quam vel augue.
-                    Sed consectetur, quam nec fringilla tincidunt, elit lectus laoreet erat, ut cursus ligula tortor in justo. 
-                    </p>
-                </div>
-                <div className={styles.imageContainer}>
-                    <img src="" alt="some project image"/>
-                </div>
-                <div className={styles.svgLogos}>
-                    <img src="" alt='svg logos'/>
-                </div>
-            </section>
-        </>
-    )
+
+const Accordion = styled((props) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+        borderBottom: 0,
+    },
+    '&::before': {
+        display: 'none',
+    },
+    flexDirection: 'row',
+}));
+
+const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+        {...props}
+    />
+))(({ theme }) => ({
+    minWidth: '150px',
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+        transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+        marginLeft: theme.spacing(1),
+    },
+    ...theme.applyStyles('dark', {
+        backgroundColor: 'rgba(255, 255, 255, .05)',
+    }),
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme.spacing(2), 
+}));
+
+export default function Projects() {
+    const [expanded, setExpanded] = React.useState('panel1');
+
+    const handleChange = (panel) => (event, newExpanded) => {
+        setExpanded(newExpanded ? panel : false);
+    };
+
+    return (
+        <div>
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                    <Typography>Collapsible Group Item #1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <img src='https://placehold.co/600x400'></img>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+                    <Typography>Collapsible Group Item #2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <img src='https://placehold.co/600x400'></img>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+                    <Typography>Collapsible Group Item #3</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <img src='https://placehold.co/600x400'></img>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
 }
